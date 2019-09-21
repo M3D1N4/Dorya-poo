445e0ff1c1bf975d7d24b522138b5716ac75a632
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-09-20 21:09:06 -0700

    Add files via upload

---
+++ Programs/Pelicula/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pelicula
+{
+    class Movie{
+        public String titulo;
+        public Int16 año;
+        public String pais;
+        public String director;}
+    
+    class Program
+    {
+
+        static void Main()
+        { 
+           Movie a;
+           a = new Movie();
+           Movie b = new Movie();
+           a.titulo = "Tangamandapio";
+           a.año = 2019;
+
+            b.titulo = "La La Land";
+           b.año = 2000;
+
+           Console.WriteLine("-Pelicula 1-");
+           Console.WriteLine("Titulo:" + a.titulo);
+           Console.WriteLine("Fecha:" + a.año);
+
+           Console.WriteLine("-Pelicula 1-");
+           Console.WriteLine("Titulo:" + b.titulo);
+           Console.WriteLine("Fecha:" + b.año);
+
+
+
+          //  Pelicula p1 = new Pelicula();
+            //p1.SetTitulo("La La Land");
+            //p1.SetAño(2016); 
+            //Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+        }
+    }
+}
