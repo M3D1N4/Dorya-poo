38a4257c2502c60df6889539e9aad9f4dc78a8b2
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-09-30 19:59:02 -0700

    Add files via upload

--- Programs/Unidad2/Parametros/Program.cs
+++ Programs/Unidad2/Parametros/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Parametros
 {
@@ -15,18 +16,34 @@ namespace Parametros
     }
 
     class Anonimo{
+
           static public void anonimiza(Programa p){
                         p.nombre = "xxx";
                         p.apellido = "xxx";
           }
-          static void main(){
+          static public void cambia(Programa p){
+                        p = new Programa();
+                        p.nombre = "Jhony";
+                        p.apellido = "Joe";
+          }
+          static public void cambia_ref(Programa p){
+                        p = new Programa();
+                        p.nombre = "Jhony";
+                        p.apellido = "Joe";
+          }
+          static void Main(){
                Programa espia = new Programa();
 
                Console.WriteLine(espia.nombre);
                anonimiza(espia);
                Console.WriteLine(espia.nombre);
+               cambia(espia);
+               Console.WriteLine(espia.nombre);
+               cambia_ref(espia);
+               Console.WriteLine(espia.nombre);
+               
           }
     }
 
-    
+
 }
