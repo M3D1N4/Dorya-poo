041212189152da10b7ce5d3621a4803064ad6886
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-09-30 19:51:39 -0700

    Add files via upload

---
+++ Programs/Unidad2/Parametros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Parametros
+{
+    class Programa
+    {
+        public string nombre;
+        public string apellido;
+
+        public Programa(){
+             nombre = "Pepito";
+             apellido = "jaimito";
+        }
+       
+    }
+
+    class Anonimo{
+          static public void anonimiza(Programa p){
+                        p.nombre = "xxx";
+                        p.apellido = "xxx";
+          }
+          static void main(){
+               Programa espia = new Programa();
+
+               Console.WriteLine(espia.nombre);
+               anonimiza(espia);
+               Console.WriteLine(espia.nombre);
+          }
+    }
+
+    
+}
