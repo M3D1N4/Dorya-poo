8c8a15afdabb80023554504fa977e64224b14594
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-10-07 18:22:35 -0700

    Add files via upload

--- Programs/Unidad2/Parametros/Program.cs
+++ Programs/Unidad2/Parametros/Program.cs
@@ -16,8 +16,21 @@ namespace Parametros
     }
 
     class Anonimo{
+                  
+          public static void anonimiza(Programa p){
+              p.nombre = "xxxx";
+          }
+          public static void anonimiza1(Programa p1){
+              p1.apellido = "xxxxx";
+          }
+          public static void cambia(ref Programa p){
+              p = new Programa();
+              p.nombre = "new";
+              p = new Programa();
+              p.apellido = "new";
+          }
 
-          static public void anonimiza(Programa p){
+         /*  static public void anonimiza(Programa p){
                         p.nombre = "xxx";
                         p.apellido = "xxx";
           }
@@ -41,7 +54,37 @@ namespace Parametros
                Console.WriteLine(espia.nombre);
                cambia_ref(espia);
                Console.WriteLine(espia.nombre);
-               
+             
+          }*/ 
+          class Program{
+              static void duplica( ref int x){
+                  x = x * 2;
+              }
+              static int suma(int x, int y){
+                  return x+y;
+              }
+              static void suma(int x, int y, out int resultado){
+                  resultado = x + y;
+              }
+              static void Main(string[]args){
+                  int a = 2;
+                  int b = 3;
+                  int r = suma(a,b);
+                  int r2;
+                  suma(a,b,out r2);
+
+                  Console.WriteLine(r);
+                  Console.WriteLine(r2);
+
+                  Programa p = new Programa();
+                  Console.WriteLine(p.nombre);
+                  Anonimo.anonimiza(p);
+                  Anonimo.cambia(ref p);
+                  Programa p1 = new Programa();
+                  Console.WriteLine(p.apellido);
+                  Anonimo.anonimiza(p);
+                  Anonimo.cambia(ref p);
+              }
           }
     }
 
