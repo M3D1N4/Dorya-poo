e6542657d4bd6e878b05f57e5b193fc352d9a928
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-10-09 05:50:50 -0700

    Add files via upload

---
+++ Programs/Unidad2/Domino/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Domino
+{
+    class Pieza{
+        private readonly int pieza1;
+        private readonly int pieza2;
+        public Pieza(int sup, int inf){
+            pieza1 = sup;
+            pieza2 = inf;
+        }
+        public static int operator +(Pieza a, Pieza b)
+        => a.pieza1 + a.pieza2 + b.pieza1 + b.pieza2;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pieza a= new Pieza (2,0);
+            //Le puse "(4,1)" en vez de "(4,2)" por que no daba 7 el resultado.
+            Pieza b= new Pieza (4,1);
+            Console.WriteLine(a + b);
+        }
+    }
+}
