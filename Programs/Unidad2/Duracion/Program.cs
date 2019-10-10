09f679a6c40c4f427c37c6f01cb4eba1ac91b218
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-10-10 00:56:22 -0700

    Add files via upload

---
+++ Programs/Unidad2/Duracion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Duracion
+{
+    class Tiempo{
+private int Horas;
+private int Minutos;
+private int Segundos;
+
+public Tiempo(int H, int M, int S){
+Horas = H;
+Minutos = M;
+Segundos = S;
+}
+public void print(){
+    Console.WriteLine(Horas);
+}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiempo Pelicula = new Tiempo(2,15,12);
+            Pelicula.print();
+            
+           
+        }
+    }
+}
