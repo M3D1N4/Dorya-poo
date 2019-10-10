bb66472757560197c3214df296f87ea56cc49292
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-10-10 13:00:13 -0700

    Add files via upload

--- Programs/Unidad2/Duracion/Program.cs
+++ Programs/Unidad2/Duracion/Program.cs
@@ -6,25 +6,32 @@ namespace Duracion
 private int Horas;
 private int Minutos;
 private int Segundos;
+private int MinutosT;
 
-public Tiempo(int H, int M, int S){
+public Tiempo(int H, int M, int S, int MT){
 Horas = H;
 Minutos = M;
 Segundos = S;
+MinutosT = MT;
 }
 public void print(){
+   
     Console.WriteLine("Horas :"+Horas+" Minutos : "+Minutos+" Segundos :"+Segundos );
+     Console.WriteLine("Minutos totales :" +((Horas*60)+Minutos) );
 }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Tiempo Pelicula = new Tiempo(2,15,12);
-            Tiempo Cancion = new Tiempo(0,02,15);
-            Tiempo Partido = new Tiempo(2,00,10);
+            Tiempo Pelicula = new Tiempo(2,15,12,0);
+            Tiempo Cancion = new Tiempo(0,02,15,0);
+            Tiempo Partido = new Tiempo(2,00,10,0);
+            Console.WriteLine("-Pelicula-");
             Pelicula.print();
+            Console.WriteLine("-Canción-");
             Cancion.print();
+            Console.WriteLine("-Partido-");
             Partido.print();
             
            
