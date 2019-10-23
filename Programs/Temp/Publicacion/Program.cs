32d907b305c6bd9324995a63b1720104f0e205d2
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-10-23 12:54:15 -0700

    Add files via upload

---
+++ Programs/Temp/Publicacion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collecion.Generic;
+
+namespace Publicacion
+{
+    class Publicacion{
+        string Usuario;
+        string Fecha;
+        string Calificacion;
+
+        public Publicacion(string Usuario, string Fecha, string Calificacion)
+        {
+            This.Usuario("Pepe");
+        }
+
+        class Estado{
+
+        }
+
+        class Foto{
+
+        }
+
+        class Link{
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Usuario);
+        }
+    }
+}
