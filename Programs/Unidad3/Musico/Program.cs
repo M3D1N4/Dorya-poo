51504c8cdd598f3634b58ec11ae5cd063549c024
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-10-26 14:28:50 -0700

    Add files via upload

--- Programs/Unidad3/Musico/Program.cs
+++ Programs/Unidad3/Musico/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Musico
 {
@@ -9,23 +11,40 @@ namespace Musico
             Nombre = N;
         }
 
-        public void Saludar(){
+        public virtual void Saludar(){
             Console.WriteLine("Hola, soy {0}",Nombre);
         }
-        public void Afinar(){
+        public virtual void Afinar(){
             Console.WriteLine("{0},Afine su instrumento {0}",Nombre);
         }
         class Bajista:Musico{
             private string Bajo;
-            public Bajista(string no,string Bajo):base(no)
+            public Bajista(string no,string B):base(no)
             {
-                this.Bajo=Bajo;
+                this.Bajo=B;
             }
-            public void Afinar1(){
-                Console.WriteLine("{0} Esta afinando su bajo {1}",Nombre,Bajo);
+            public override void Afinar(){
+                Console.WriteLine("{0} Afinando su {1}",Nombre,Bajo);
+            }
+            /*public void Afinar1(){
+                Console.WriteLine("{0} Esta afinando su bajo {1}",Nombre,B);
             }
             public void Tocar(){
-                Console.WriteLine("{0} Esta utilizando su bajo {1}",Nombre,Bajo);
+                Console.WriteLine("{0} Esta utilizando su bajo {1}",Nombre,B);
+            }
+             */
+        }
+        class Guitarrista:Musico{
+            private string Guitarra;
+            public Guitarrista(string no, string Guitarra):base(no)
+            {
+                this.Guitarra=Guitarra;
+            }
+            public void Afinar2(){
+                Console.WriteLine("{0} Esta afinando su Guitarra {1}",Nombre,Guitarra);
+            }
+            public void Tocar1(){
+                Console.WriteLine("{0} Esta utilizando su guitarra {1}",Nombre,Guitarra);
             }
         }
     }
@@ -34,7 +53,23 @@ namespace Musico
         static void Main(string[] args)
         {
             Musico Tomas=new Musico("Tomas");
-            Tomas.Saludar();
+            Bajista flea=new Bajista("Flea","MusicMan");
+            //Guitarrista JSenoue=new Guitarrista("JSenoue","Call me crazy");
+            Console.WriteLine(Tomas.Saludar());
+            Tomas.Afinar();flea.Afinar();
+
+            List <Musico> grupo = new List <Musico>();
+            grupo.Add(Tomas);
+            grupo.Add(flea);
+            foreach(Musico m in grupo){
+                Console.WriteLine(m.Saludar());
+                m.Afinar();
+            }
+          
+           
+            
+           
+            
         }
     }
 }
