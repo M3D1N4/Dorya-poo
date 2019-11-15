ebdb806ff0bb6771b325a6dfb93ab17e2fd1ccf7
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-11-14 19:18:06 -0800

    Add files via upload

--- Programs/Temp/Publicacion/Program.cs
+++ Programs/Temp/Publicacion/Program.cs
@@ -7,44 +7,48 @@ namespace Publicacion
     abstract class Publicacion{
        protected string Nombre;
        protected string Mensaje;
-       protected string amigo;
-       protected string comentario;
-       protected string link;
+       protected string Amigo;
+       protected string Comentario;
+       protected string Link;
        
        public Publicacion(string N,string M,string A,string C,string L)
        {
            this.Nombre = N;
            this.Mensaje = M;
-           this.amigo = A;
-           this.comentario = C;
-           this.link = L;
+           this.Amigo = A;
+           this.Comentario = C;
+           this.Link = L;
        }
      public abstract void Print();
     }
 
     class Estado:Publicacion{
-      public Estado(string N,string M,string A,string C,string L):base(N,M,A,C,L)
-        {
+      public Estado(string N,string M,string A,string C,string L):base(N,M,A,C,L){
           
         }
-        public override void Print()
-        {
-              Console.WriteLine("By: {0} \n Message :{1} \n Reacts: {2}\n Comments : {3}\n Link {4} \n (status)Les deseo Felices fiestas\n",Nombre,Mensaje,amigo,comentario,link);
+        public override void Print(){
+              Console.WriteLine("By: {0} \n Message :{1} \n Reacts: {2}\n Comments : {3}\n Link {4} \n (status)Les deseo Felices fiestas\n",Nombre,Mensaje,Amigo,Comentario,Link);
    
         }
     }
-    class Fotos{
+    class Fotos:Publicacion{
+        public Fotos(string N,string M,string A,string C,string L):base(N,M,A,C,L){
+            
+        }
+        public override void Print(){
+           Console.WriteLine("By: {0} \n Message :{1} \n Reacts: {2}\n Comments : {3}\n Link {4} \n (status)Desperdiciando oxigeno\n",Nombre,Mensaje,Amigo,Comentario,Link);
+        }
     }
     class Link{
 
     }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-          var X = new Estado("Jorge Nitales","Ami me wusta el tangananica","1 DisLike","Se escribe con g.","feisbuk.com");
+    class Program{
+        static void Main(string[] args){
+          var X = new Estado("Jorge Nitales","Ami me wusta el tangananica","1 DisLike","Se escribe con g.","www.feisbuk.com");
            X.Print();
+           var Y = new Estado("Carlos Santoyo","Solo quiero demostrar que todos estan equivocados y yo tengo razón."," 27 Likes, 500 DisLike","Exclamo la princesa.","www.Cara-libro.com");
+           Y.Print();
         }
     }
 }
