116437d5012c4d496e53c0842f6238944e7f2f93
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-11-14 18:24:30 -0800

    Add files via upload

---
+++ Programs/Temp/Publicacion/Publicacion/Program.cs
@@ -0,0 +1,43 @@
+﻿  
+using System;
+using System.Collections.Generic;
+
+namespace Publicacion
+{
+    abstract class Publicacion
+    {
+       
+       protected string Nombre;
+       protected string Mensaje;
+       protected string amigo;
+       protected string comentario;
+       protected string link;
+       
+       public Publicacion(string N,string M,string A,string C,string L)
+       {
+           this.Nombre = N;
+           this.Mensaje = M;
+           this.amigo = A;
+           this.comentario = C;
+           this.link = L;
+       }
+ 
+    }
+
+    class Estado{
+      
+    }
+    class Fotos{
+    }
+    class Link{
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
