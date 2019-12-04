45823978decc52f54dbae0772b6722c6eda2f144
M3D1N4 <44390904+M3D1N4@users.noreply.github.com>
2019-12-03 18:33:29 -0800

    Add files via upload

---
+++ Programs/Unidad5/Cola/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cola
+{
+    class Program{
+        static void Main(string[] args){            
+            //  Código Queue que contiene los elementos en la lista "row"  //
+            Queue <string> row = new Queue <string>();
+                row.Enqueue("Yeah");
+                 row.Enqueue("Cool");
+                  row.Enqueue("AWESOME!");
+                   row.Enqueue("OUTSTANDING!");
+                    row.Enqueue("AMAZING!");
+                     row.Enqueue("Failure");
+                      row.Enqueue("Try again");
+
+            Console.WriteLine("-. Hay {0} elementos en este momento, son los siguientes: .-", row.Count);
+            //   Muestra los elementos en la cola 1 por 1 en orden(FIFO)   //
+            foreach(string fila in row){
+                Console.WriteLine(fila);
+            }
+            Console.ReadLine();
+        }
+    }
+}
